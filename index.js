2c6f17066a41865f397ff9326f6ad090a5cda9ed
LSteven10 <laurenmontenegro10@gmail.com>
2017-10-22 22:07:41 -0600

    Iniciando un servidor Express de GraphQL

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const graphqlExpres = require('apollo-server-express');
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
 
-//Declaración del puerto
-const PORT = 3000;
-//Instanciamos express para poder usarlo
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
 var app = express();
-
-
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphQLSchema }));
-app.listen(PORT);
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
